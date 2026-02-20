8d3eb29bd080291784419c7f449cad7c08cbd47c
Mohammed Zain ul Hassan <powehi29@gmail.com>
2026-02-20 11:18:31 +0500

    fix: allow users to update their own record to see API keys

--- src/collections/Users/access/updateAndDelete.ts
+++ src/collections/Users/access/updateAndDelete.ts
@@ -1,9 +1,10 @@
-import type { Access } from 'payload'
+import type { Access, Where } from 'payload'
+import type { User } from '@/payload-types'
 
 import { isSuperAdmin } from '../../../access/isSuperAdmin'
 import { getTenantAdminTenantAccessIDs } from '../../../utilities/getTenantAccessIDs'
 
-export const updateAndDeleteAccess: Access = (args) => {
+export const updateAndDeleteAccess: Access<User> = (args) => {
   const { req } = args
   if (!req.user) {
     return false
@@ -16,8 +17,17 @@ export const updateAndDeleteAccess: Access = (args) => {
   const adminTenantAccessIDs = getTenantAdminTenantAccessIDs(req.user)
 
   return {
-    'tenants.tenant': {
-      in: adminTenantAccessIDs,
-    },
-  }
+    or: [
+      {
+        id: {
+          equals: req.user.id,
+        },
+      },
+      {
+        'tenants.tenant': {
+          in: adminTenantAccessIDs,
+        },
+      },
+    ],
+  } as Where
 }
